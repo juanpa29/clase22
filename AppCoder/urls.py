7b3cda78625e5d87bff690a23b6e8a76879f2521
profe <dalevdream@gmail.com>
2023-09-26 21:31:26 -0500

    clase 23

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -7,20 +7,32 @@ urlpatterns = [
 
     #urls de leer
     path("cursos/", ver_cursos, name="VerCursos"),
-    path("profes/", ver_profes, name="VerProfes"),
     path("entregas/", ver_entregras, name="VerEntregas"),
     path("estudiantes/", ver_estudiante, name="VerEstudiantes"),
 
     #urls de crear
     path("crear_curso/", crear_cursos, name="CrearCursos"),
-    path("crear_profe/", crear_profes, name="CrearProfes"),
 
     #urls de búsqueda
-    path("buscar_profe/", buscar_profes, name="BuscarProfes"),
     path("resu_profe/", resultado_profes, name="ResultadoProfes"),
 
     #urls de borrar
+
+    #CRUD de profesor
+    path("profes/", ver_profes, name="VerProfes"),
+    path("buscar_profe/", buscar_profes, name="BuscarProfes"),
+    path("crear_profe/", crear_profes, name="CrearProfes"),
     path("borrar_profe/<profesor_nombre>", eliminar_profesor, name="BorrarProfe"),
+    path("update_profe/<profesor_nombre>", actualizar_profesor, name="Actualizar Profesor"),
 
 
+    #CRUD de estudiante con vistas usando Clases
+    path("estudiantes/lista/", EstudianteLista.as_view(), name="Ver Estudiantes"),
+    path("estudiantes/detalle/<int:pk>", EstudianteDetalle.as_view(), name="Detalle Estudiantes"),
+    path("estudiantes/nuevo/", EstudianteCrear.as_view(), name="Crear Estudiantes"),
+    path("estudiantes/actualizar/<int:pk>", EstudianteActualizar.as_view(), name="Actualizar Estudiantes"),
+    path("estudiantes/borrar/<int:pk>", EstudianteBorrar.as_view(), name="Borrar Estudiantes"),
+
+
+    path("login/", login_view, name="Inicio Sesion"),
 ]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Profesor, Curso
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from .models import Profesor, Curso, Estudiante
 from .forms import ProfesorFormulario
 
 # Create your views here.
@@ -101,12 +106,33 @@ def eliminar_profesor(request, profesor_nombre):
 
 
 
-
-
-
-
 def actualizar_profesor(request, profesor_nombre):
-    pass
+
+    profesor_escogido = Profesor.objects.get(nombre=profesor_nombre)
+    
+    if request.method == "POST": #hacemos click al botón actualizar
+
+        miFormulario = ProfesorFormulario(request.POST) # Aqui me llega la informacion del html 
+
+        if miFormulario.is_valid():
+                
+                info = miFormulario.cleaned_data
+                
+                profesor_escogido.nombre = info["nombre"]
+                profesor_escogido.apellido = info["apellido"]
+                profesor_escogido.email = info["email"]
+                profesor_escogido.profesion = info["profesion"]
+
+                profesor_escogido.save()
+
+                return render(request, "AppCoder/inicio.html")
+    else:
+        miFormulario = ProfesorFormulario(initial={"nombre":profesor_escogido.nombre,
+                                                   "apellido":profesor_escogido.apellido,
+                                                   "email":profesor_escogido.email,
+                                                   "profesion":profesor_escogido.profesion}) #mostrar formulario con la informacion previa
+
+        return render(request, "AppCoder/editar_profes.html", {"form": miFormulario})
 
 
 
@@ -118,3 +144,75 @@ def actualizar_profesor(request, profesor_nombre):
 def ver_cursos(request):
     todos = Curso.objects.all()
     return render(request, "AppCoder/ver_cursos.html", {"cursos":todos})
+
+
+
+
+
+def login_view(request):
+
+    if request.method == "POST": #click al boton iniciar sesion
+
+        form_inicio = AuthenticationForm(request, data = request.POST)
+        
+        if form_inicio.is_valid(): #el formulario nos ayuda a validar
+
+            info = form_inicio.cleaned_data #data que se escribio en el formulario de login en modo diccionario 
+            usuario = info.get("username")
+            contra = info.get("password")
+
+            #acá hacemos la validación
+            user = authenticate(username=usuario, password=contra) #existe el usuario (retorna el usuario) ---- no existe usuario (retorna None)
+
+            if user:
+                login(request, user)    #iniciar sesion ya que el usuario si existe (credenciales correctas)
+                return render(request, "AppCoder/inicio.html", {"usuario":user})
+        
+        else:
+            return render(request,"AppCoder/inicio.html", {"mensaje":"DATOS INCORRECTOS"})
+
+    form_inicio = AuthenticationForm() #formulario vacio
+
+    return render(request,"AppCoder/login.html", {"form":form_inicio} )
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Vista basada en clases  --- CRUD estudiantes
+
+#Read 
+class EstudianteLista(ListView): #estudiante_list.html
+    model = Estudiante
+    template_name = "AppCoder/LISTA_ESTUDIANTES.html"
+
+#Detail 
+class EstudianteDetalle(DetailView): #estudiante_detail.html
+    model = Estudiante
+    
+#Create
+class EstudianteCrear(CreateView): #estudiante_form.html
+    model = Estudiante
+    fields = ["nombre", "apellido", "email"]
+    success_url = "/AC/estudiantes/lista/"
+
+#Update
+class EstudianteActualizar(UpdateView): #estudiante_form.html
+    model = Estudiante
+    fields = ["nombre", "apellido", "email"]
+    success_url = "/AC/estudiantes/lista/"
+
+#Delete
+class EstudianteBorrar(DeleteView): #estudiante_confirm_delete.html
+    model = Estudiante 
+    success_url = "/AC/estudiantes/lista/"
+
+
